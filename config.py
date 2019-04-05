11c3952a2edb5be90a52141855e6d255fd97ff55
Nathan Thillairajah <nthillairajah@yext.com>
2019-04-05 17:26:46 -0400

    remove common credentials, add config.py to .gitignore
    we were getting rate-limited by Twitter
    
    🔥🌲😩😩

--- config.py
+++
@@ -1,5 +0,0 @@
-# NOTE: these tokens are for a test app that was created specifically for the hackathon
-consumer_key = "6mge7HJJGRcgkw95RlZ7bxCzI"
-consumer_secret = "bqpHPN00mC66kIJhW1o8DwXvbSlqSv1a9ycul0ijJloVDPML8V"
-access_token = "1112782890486718469-dKs9N6vS9RuciEJoU5uFCeCT3E9zGN"
-access_token_secret = "DieWhafUO85y5oNuhoW4mBotAAGQSQ2HEC9qPeMoA3IWH"
